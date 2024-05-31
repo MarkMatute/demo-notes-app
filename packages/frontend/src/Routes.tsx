1b73f36c8ca4ad1f8de7d9ba87866c9f5e5d8a83
Mark Ernest R. Matute <markernest.matute@gmail.com>
2024-05-31 15:21:56 +0800

    Added update notes

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -4,12 +4,14 @@ import NotFound from "./containers/NotFound.tsx";
 import Login from "./containers/Login.tsx";
 import Signup from "./containers/Signup.tsx";
 import NewNote from "./containers/NewNote.tsx";
+import Notes from "./containers/Notes.tsx";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/:id" element={<Notes />} />
       <Route path="/notes/new" element={<NewNote />} />
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />;
--- packages/functions/src/update.ts
+++ packages/functions/src/update.ts
@@ -8,20 +8,14 @@ export const main = handler(async (event) => {
   const params = {
     TableName: Table.Notes.tableName,
     Key: {
-      // The attributes of the item to be created
-      userId: "123", // The id of the author
-      noteId: event?.pathParameters?.id, // The id of the note from the path
+      userId: event.requestContext.authorizer?.iam.cognitoIdentity.identityId,
+      noteId: event?.pathParameters?.id,
     },
-    // 'UpdateExpression' defines the attributes to be updated
-    // 'ExpressionAttributeValues' defines the value in the update expression
     UpdateExpression: "SET content = :content, attachment = :attachment",
     ExpressionAttributeValues: {
       ":attachment": data.attachment || null,
       ":content": data.content || null,
     },
-    // 'ReturnValues' specifies if and how to return the item's attributes,
-    // where ALL_NEW returns all attributes of the item after the update; you
-    // can inspect 'result' below to see how it works with different settings
     ReturnValues: "ALL_NEW",
   };
 
